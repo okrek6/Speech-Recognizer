224d45da6271d9ba3af7ce3c3971c4676fece1ef
Brendan Krekeler <bfkyb4@mail.missouri.edu>
2019-02-10 02:30:02 -0600

    Correct commit

---
+++ Speech Recognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Speech Recognition
+//
+//  Created by Brendan Krekeler on 2/10/19.
+//  Copyright © 2019 Brendan Krekeler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
