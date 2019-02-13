0c5ce3c46b33dcd8d0ff0ebee40be400ab972301
Brendan Krekeler <bfkyb4@mail.missouri.edu>
2019-02-13 15:02:00 -0600

    hello

--- Speech Recognition/ViewController.swift
+++ Speech Recognition/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
+    //this is a test for a student
 
 }
 
